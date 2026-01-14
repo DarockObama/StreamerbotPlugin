3da9e015bbcc118d5df2d36a44d52ab7fc5e0f74
DarockObama <therockobama75@gmail.com>
2026-01-14 17:56:35 +0100

    ...

--- src/main/java/streamerbot/messaging/TriggerHandler.java
+++ src/main/java/streamerbot/messaging/TriggerHandler.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -17,24 +18,37 @@ public class TriggerHandler {
     private OkHttpClient okHttpClient;
 
     public void sendJson(String payload) {
-        if(StringUtils.isBlank(config.streamerbotAddress())) {
+        if (StringUtils.isBlank(config.streamerbotAddress())) {
             log.error("No address provided");
+            return; // important: don't continue
         }
 
-        RequestBody body = RequestBody.create(MediaType.parse("application/json"), payload);
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json"),
+                payload
+        );
+
         Request request = new Request.Builder()
                 .url(config.streamerbotAddress() + "/DoAction")
                 .post(body)
                 .build();
 
-        try (Response response = okHttpClient.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                log.error("POST request failed. Response Code: {}", response.code());
-            } else {
-                log.info("POST request succeeded!");
+        okHttpClient.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(@Nonnull okhttp3.Call call,@Nonnull  java.io.IOException e) {
+                log.error("Error during POST request", e);
             }
-        } catch (Exception e) {
-            log.error("Error during POST request", e);
-        }
+
+            @Override
+            public void onResponse(@Nonnull okhttp3.Call call, @Nonnull okhttp3.Response response) {
+                try (okhttp3.Response res = response) {
+                    if (!res.isSuccessful()) {
+                        log.error("POST request failed. Response Code: {}", res.code());
+                    } else {
+                        log.info("POST request succeeded!");
+                    }
+                }
+            }
+        });
     }
 }
